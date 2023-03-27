ac58a393911ee4f82e977f69c41be3b50f3b65cb
austinoso <contact@austinosor.io>
2023-03-27 12:22:35 +0000

    init vite project added tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
