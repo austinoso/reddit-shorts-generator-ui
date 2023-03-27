8a65f2ad9666b86cdf3fc5cf1151b4a731c962d2
austinoso <contact@austinosor.io>
2023-03-27 12:36:00 +0000

    setup tailwind ui and basic layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import Layout from "./components/common/Layout";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Layout>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </Layout>
     </div>
   );
 }
