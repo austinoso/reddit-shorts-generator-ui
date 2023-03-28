b88807ef673daa7ff69ed81b33a2a52fc362a9e5
austinoso <contact@austinosor.io>
2023-03-28 22:59:53 +0000

    setup react router and install tailwind forms  * route for "/": video table  * route for "/videos" detail video page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,26 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Layout from "./components/common/Layout";
+import VideoTable from "./components/VideoTable";
+import VideoForm from "./components/VideoForm";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <VideoTable />,
+  },
+  {
+    path: "/videos/:id",
+    element: <VideoForm />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <RouterProvider router={router} />
       </Layout>
     </div>
   );
