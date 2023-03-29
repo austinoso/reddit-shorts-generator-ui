b558d93f2534434c6ae28a5367e18362facb07af
austinoso <contact@austinosor.io>
2023-03-29 20:37:23 +0000

    ws client  * use react-use-websocket to connect to server ws * use video context in app comp * if video generator done, video on client to "complete" and enable   video actions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,8 @@ import Layout from "./components/common/Layout";
 import VideoTable from "./components/VideoTable";
 import VideoForm from "./components/VideoForm";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { VideoProvider } from "./contexts/videos";
+import useWebSocket from "react-use-websocket";
+import { useVideoContext } from "./contexts/videos";
 
 const router = createBrowserRouter([
   {
@@ -18,14 +19,43 @@ const router = createBrowserRouter([
   },
 ]);
 
+const WS_URL = "ws://localhost:7778";
+
 function App() {
+  const { videos, setVideos } = useVideoContext();
+
+  const handleGenerateVideoMessage = (messageData: any) => {
+    const { status, videoId, error } = messageData;
+
+    const video = videos.find((video) => video._id === videoId);
+    console.log("video", video);
+    if (!video) return;
+
+    if (status === "closed") {
+      video.status = "complete";
+    }
+
+    console.log("videos", videos);
+
+    setVideos([...videos]);
+  };
+
+  useWebSocket(WS_URL, {
+    onOpen: () => console.log("opened"),
+    onClose: () => console.log("closed"),
+    onMessage: (event) => {
+      const data = JSON.parse(event.data);
+      console.log(data);
+
+      if (data.type === "generateVideo") handleGenerateVideoMessage(data.data);
+    },
+  });
+
   return (
     <div className="App">
-      <VideoProvider>
-        <Layout>
-          <RouterProvider router={router} />
-        </Layout>
-      </VideoProvider>
+      <Layout>
+        <RouterProvider router={router} />
+      </Layout>
     </div>
   );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { VideoProvider } from "./contexts/videos";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <VideoProvider>
+      <App />
+    </VideoProvider>
+  </React.StrictMode>
+);
