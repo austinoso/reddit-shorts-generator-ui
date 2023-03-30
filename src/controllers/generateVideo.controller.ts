b05f44e7bb8b45f4510a16d57e483054250ecb9b
austinoso <contact@austinosor.io>
2023-03-30 09:49:51 +0000

    return error

--- src/controllers/generateVideo.controller.ts
+++ src/controllers/generateVideo.controller.ts
@@ -5,7 +5,6 @@ export async function process(req: Request, res: Response, next: NextFunction) {
   try {
     const { videoId } = req.body;
     const worker = await startWorker(videoId);
-    worker.pid;
     res.json({ message: "Added video to queue", workerId: worker.pid });
   } catch (e) {
     console.log("Error creating video: ", e);
@@ -16,8 +15,9 @@ export async function process(req: Request, res: Response, next: NextFunction) {
 export async function stop(req: Request, res: Response, next: NextFunction) {
   try {
     await stopWorker();
-    res.json({ message: "Stopped video worker" });
+    res.json({ message: "Stopped video worker", error: null });
   } catch (e) {
+    res.json({ message: "Error stopping video worker", error: e });
     console.log("Error stopping video worker: ", e);
     next(e);
   }
