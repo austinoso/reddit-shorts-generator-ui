3f9ad1e65451618dcadc53ee6873bb608d279c5a
austinoso <contact@austinosor.io>
2023-03-30 06:11:52 +0000

    send request with post data to video object

--- videoGenerator/src/generateVideo.ts
+++ videoGenerator/src/generateVideo.ts
@@ -1,8 +1,15 @@
 import { getPostData } from "./utils/reddit/posts.js";
 import { editVideo, buildEditSpec } from "./utils/editor/editor.js";
+import axios from "axios";
 
 export async function generateVideo(postUrl) {
   const post = await getPostData(postUrl);
+  const videoId = process.env.VIDEO_ID;
+
+  await axios.put(`http://localhost:7777/api/videos/${videoId}`, {
+    title: post.title.text,
+    postId: post.id,
+  });
 
   // edit video
   const editSpec = await buildEditSpec(post);
