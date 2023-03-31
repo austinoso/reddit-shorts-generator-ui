5bc9e938c3713e953b3dd7330cde5f8f54455c7b
austinoso <contact@austinosor.io>
2023-03-31 07:55:37 +0000

    parse data percent progress send progress with sendWSMessage

--- workers/generateVideo.worker.ts
+++ workers/generateVideo.worker.ts
@@ -1,4 +1,5 @@
 import { spawn } from "child_process";
+import { sendWSMessage } from "../src/wsServer";
 
 async function createWorker(postUrl: string, id: string) {
   const child = spawn(
@@ -6,7 +7,7 @@ async function createWorker(postUrl: string, id: string) {
       __dirname +
       "/../" +
       " && " +
-      `POST=${postUrl} VIDEO_ID=${id} node videoGenerator/dist/main.js`,
+      `POST=${postUrl} VIDEO_BACKGROUND=./assets/mc-bkg-video.mp4 VIDEO_ID=${id} node videoGenerator/dist/main.js`,
     { detached: true, shell: true }
   );
 
@@ -14,7 +15,21 @@ async function createWorker(postUrl: string, id: string) {
   console.log("child process pid: ", child.pid);
 
   child.stdout.on("data", (data: any) => {
-    console.log(`stdout: ${data}`);
+    let progress = data.toString().trim();
+    if (progress.endsWith("%")) {
+      // remove the % sign
+      progress = progress.slice(0, -1);
+      console.log("progress: ", progress);
+
+      sendWSMessage({
+        type: "generateVideoProgress",
+        data: {
+          progress: progress,
+        },
+      });
+    } else {
+      console.log("data: ", data.toString());
+    }
   });
 
   child.stderr.on("data", (data: any) => {
